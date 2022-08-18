e8ecb857b34221e55de5b225e3134d6e19d03821
yeongseokim <1903020@ewhain.net>
2022-08-18 14:29:17 +0900

    ui style 수정

--- src/navigations/Tab.js
+++ src/navigations/Tab.js
@@ -2,16 +2,16 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Calender } from "../screens/CalenderScreen";
 import { Chat } from "../screens/ChatScreen";
-import  Lens  from "../screens/LensScreenT";
+import { Lens } from "../screens/LenScreen";
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
-    return(
+    return (
         <Tab.Navigator>
-            <Tab.Screen name ="Calender" component = {Calender} />
-            <Tab.Screen name ="Chat" component = {Chat} />
-            <Tab.Screen name ="Lens" component = {Lens}/>
+            <Tab.Screen name="Calender" component={Calender} />
+            <Tab.Screen name="Chat" component={Chat} />
+            <Tab.Screen name="Lens" component={Lens} />
         </Tab.Navigator>
     );
 };
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -5,7 +5,7 @@ import styled from 'styled-components/native';
 const Container = styled.View`
   flex: 1;
   justify-content: center;
-  align-items: center;
+  align=items: center;
 `;
 const StyledText = styled.Text`
   font-size: 30px;
