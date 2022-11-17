f120dfc81fa2ce99d3f0f7eb797420737fa0b646
yeongseokim <1903020@ewhain.net>
2022-11-17 15:44:36 +0900

    메인 이미지 변경, 챗봇 이름 -> Veganee

--- src/screens/CalenderScreen.js
+++ src/screens/CalenderScreen.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import styled from 'styled-components/native';
-import {View, Dimensions, StyleSheet, ImageBackground, Text} from 'react-native';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
-import {LocaleConfig} from 'react-native-calendars';
+import { View, Dimensions, StyleSheet, ImageBackground, Text } from 'react-native';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { LocaleConfig } from 'react-native-calendars';
 import CalendarHeader from 'react-native-calendars/src/calendar/header';
 import Icon from 'react-native-vector-icons/AntDesign';
 
@@ -15,36 +15,36 @@ const Container = styled.View`
 `;
 
 LocaleConfig.locales['fr'] = {
-  monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
-  monthNamesShort: ['Janv.','Févr.','Mars','Avril','Mai','Juin','Juil.','Août','Sept.','Oct.','Nov.','Déc.'],
-  dayNames: ['일요일','월요일', '화요일','수요일','목요일','금요일','토요일'],
-  dayNamesShort: ['일', '월','화','수','목','금','토'],
+  monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+  monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+  dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+  dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
   today: 'Aujourd\'hui'
 };
 LocaleConfig.defaultLocale = 'fr';
 
 const styles = StyleSheet.create({
-    header : {
-      flex : 1.15
-    },
-    headerTextView : {
-      padding : 30
-    },
-    headerText : {
-      fontSize : 25,
-      color:'white',
-    },
-    calendar : {
-      flex : 5,
-      backgroundColor: 'white',
-      padding : 1
-    },
-    bgImage : {
-      width : '100%', height : '100%',
-      flexDirection : 'row',
-      alignItems : 'center',
-      justifyContent : 'flex-end',
-    }
+  header: {
+    flex: 1.15
+  },
+  headerTextView: {
+    padding: 30
+  },
+  headerText: {
+    fontSize: 25,
+    color: 'white',
+  },
+  calendar: {
+    flex: 5,
+    backgroundColor: 'white',
+    padding: 1
+  },
+  bgImage: {
+    width: '100%', height: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  }
 
 
 })
@@ -82,112 +82,112 @@ export const CalenderApp = () => {
     }
   };
 
-    return(
-      <Container>
+  return (
+    <Container>
 
-        <View style={styles.header}>
-          <ImageBackground source={require('../screens/hd-wallpaper-176722.jpg')} style={styles.bgImage}>
-            <View style={styles.headerTextView}>
-              <Text style={styles.headerText}>페스코 베지테리언</Text>
-              <Text style={styles.headerText}>686일째</Text>
-            </View>
-          </ImageBackground>
-        </View>
+      <View style={styles.header}>
+        <ImageBackground source={require('./cal-img1.jpg')} style={styles.bgImage}>
+          <View style={styles.headerTextView}>
+            <Text style={styles.headerText}>페스코 베지테리언</Text>
+            <Text style={styles.headerText}>686일째</Text>
+          </View>
+        </ImageBackground>
+      </View>
 
-        <View style={ styles.calendar }>
-          <Calendar 
-            markingType={'custom'}
-            markedDates={markedDates}         
-            
-            theme={{
-              'stylesheet.day.basic':{
-                'base':{
-                  width : 30, 
-                  height: 73
-                }
+      <View style={styles.calendar}>
+        <Calendar
+          markingType={'custom'}
+          markedDates={markedDates}
+
+          theme={{
+            'stylesheet.day.basic': {
+              'base': {
+                width: 30,
+                height: 73
+              }
+            },
+            'stylesheet.calendar.header': {
+              dayTextAtIndex0: {
+                color: 'red'
               },
-              'stylesheet.calendar.header': {
-                dayTextAtIndex0: {
-                  color: 'red'
-                },
-                dayTextAtIndex6: {
-                  color: 'blue'
-                }
-              },
-              backgroundColor: '#ffffff',
-              calendarBackground: '#ffffff',
-              textSectionTitleColor: '#b6c1cd',
-              textSectionTitleDisabledColor: '#d9e1e8',
-              selectedDayTextColor: '#ffffff',
-              todayTextColor: '#009688',
-              dayTextColor: '#2d4150',
-              textDisabledColor: '#d9e1e8',
-              arrowColor: '#81CC7E',
-              disabledArrowColor: '#d9e1e8',
-              monthTextColor: 'black',
-              indicatorColor: 'blue',
-              textDayFontFamily: 'monospace',
-              textMonthFontFamily: 'monospace',
-              textDayHeaderFontFamily: 'monospace',
-              textDayFontWeight: '300',
-              textMonthFontWeight: 'bold',
-              textDayHeaderFontWeight: '300',
-              textDayFontSize: 16,
-              textMonthFontSize: 16,
-              textDayHeaderFontSize: 16
-            }}
+              dayTextAtIndex6: {
+                color: 'blue'
+              }
+            },
+            backgroundColor: '#ffffff',
+            calendarBackground: '#ffffff',
+            textSectionTitleColor: '#b6c1cd',
+            textSectionTitleDisabledColor: '#d9e1e8',
+            selectedDayTextColor: '#ffffff',
+            todayTextColor: '#009688',
+            dayTextColor: '#2d4150',
+            textDisabledColor: '#d9e1e8',
+            arrowColor: '#81CC7E',
+            disabledArrowColor: '#d9e1e8',
+            monthTextColor: 'black',
+            indicatorColor: 'blue',
+            textDayFontFamily: 'monospace',
+            textMonthFontFamily: 'monospace',
+            textDayHeaderFontFamily: 'monospace',
+            textDayFontWeight: '300',
+            textMonthFontWeight: 'bold',
+            textDayHeaderFontWeight: '300',
+            textDayFontSize: 16,
+            textMonthFontSize: 16,
+            textDayHeaderFontSize: 16
+          }}
 
 
-            // Initially visible month. Default = Date()
-            current={ Date() }
-            // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-            minDate={'2020-01-01'}
-            // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-            maxDate={'2030-12-31'}
-            // Handler which gets executed on day press. Default = undefined
-            onDayPress={(day) => {setModalVisible(true);}}
-            // Handler which gets executed on day long press. Default = undefined
-            onDayLongPress={(day) => {console.log('selected day', day)}}
-            // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-            monthFormat={'yyyy MM'}
-            // Handler which gets executed when visible month changes in calendar. Default = undefined
-            onMonthChange={(month) => {console.log('month changed', month)}}
-            // Hide month navigation arrows. Default = false
-            hideArrows={false}
-            // Replace default arrows with custom ones (direction can be 'left' or 'right')
-            renderArrow={(direction) => direction === "left" ? (
-              <Icon name="caretleft" size={20} color="#81CC7E" />
-                ) : (
-              <Icon name="caretright" size={20} color="#81CC7E" />
-                )
-            }
-            // Do not show days of other months in month page. Default = false
-            hideExtraDays={true}
-            // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
-            // day from another month that is visible in calendar page. Default = false
-            disableMonthChange={true}
-            // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
-            firstDay={7}
-            // Hide day names. Default = false
-            hideDayNames={false}
-            // Show week numbers to the left. Default = false
-            showWeekNumbers={false}
-            // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-            onPressArrowLeft={substractMonth => substractMonth()}
-            // Handler which gets executed when press arrow icon right. It receive a callback can go next month
-            onPressArrowRight={addMonth => addMonth()}
-            // Disable left arrow. Default = false
-            disableArrowLeft={false}
-            // Disable right arrow. Default = false
-            disableArrowRight={false}
-            // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
-            disableAllTouchEventsForDisabledDays={true}
-            /** Replace default month and year title with custom one. the function receive a date as parameter. */
-            //renderHeader={(date) => {/*Return JSX*/}}
-          />
-        </View>
-        
-      </Container>
+          // Initially visible month. Default = Date()
+          current={Date()}
+          // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+          minDate={'2020-01-01'}
+          // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+          maxDate={'2030-12-31'}
+          // Handler which gets executed on day press. Default = undefined
+          onDayPress={(day) => { setModalVisible(true); }}
+          // Handler which gets executed on day long press. Default = undefined
+          onDayLongPress={(day) => { console.log('selected day', day) }}
+          // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+          monthFormat={'yyyy MM'}
+          // Handler which gets executed when visible month changes in calendar. Default = undefined
+          onMonthChange={(month) => { console.log('month changed', month) }}
+          // Hide month navigation arrows. Default = false
+          hideArrows={false}
+          // Replace default arrows with custom ones (direction can be 'left' or 'right')
+          renderArrow={(direction) => direction === "left" ? (
+            <Icon name="caretleft" size={20} color="#81CC7E" />
+          ) : (
+            <Icon name="caretright" size={20} color="#81CC7E" />
+          )
+          }
+          // Do not show days of other months in month page. Default = false
+          hideExtraDays={true}
+          // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+          // day from another month that is visible in calendar page. Default = false
+          disableMonthChange={true}
+          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+          firstDay={7}
+          // Hide day names. Default = false
+          hideDayNames={false}
+          // Show week numbers to the left. Default = false
+          showWeekNumbers={false}
+          // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+          onPressArrowLeft={substractMonth => substractMonth()}
+          // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+          onPressArrowRight={addMonth => addMonth()}
+          // Disable left arrow. Default = false
+          disableArrowLeft={false}
+          // Disable right arrow. Default = false
+          disableArrowRight={false}
+          // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
+          disableAllTouchEventsForDisabledDays={true}
+        /** Replace default month and year title with custom one. the function receive a date as parameter. */
+        //renderHeader={(date) => {/*Return JSX*/}}
+        />
+      </View>
 
-    );
+    </Container>
+
+  );
 };
--- src/screens/RootStack.js
+++ src/screens/RootStack.js
@@ -1,7 +1,7 @@
 import React from "react";
-import {createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import TabNavigation from "./TabScreen";
-import {Chat} from "../screens/ChatScreen";
+import { Chat } from "../screens/ChatScreen";
 
 // import CameraStackScreen from "./CameraStack";
 import RectangleScannerScreen from "../components/RectangleScannerScreen";
@@ -10,69 +10,69 @@ const Stack = createStackNavigator();
 const CameraStack = createStackNavigator();
 const ChatStack = createStackNavigator();
 
-function RootStack(){
-    return(
-        <Stack.Navigator>
-          <Stack.Screen
-            name = "BottomTab"
-            component = {TabNavigation}
-            options={({navigation, route}) => ({
-                headerShown: false,
-              })}
-           />
-           <Stack.Screen
-            name = "CameraScreens"
-            component = {CameraStackScreen}
-            options={({navigation, route}) => ({
-                headerShown: false,
-              })}
-           /> 
-           <Stack.Screen
-            name = "ChatScreen"
-            component = {ChatStackScreen}
-            options = {({navigation, route}) => ({
-              headerShown: true,
-              headerTitleAlign: "center",
-              headerTitleStyle: {
-                fontSize: 15
-              },
-              headerTitle: "챗봇 이름",
-            })}
-           />           
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="BottomTab"
+        component={TabNavigation}
+        options={({ navigation, route }) => ({
+          headerShown: false,
+        })}
+      />
+      <Stack.Screen
+        name="CameraScreens"
+        component={CameraStackScreen}
+        options={({ navigation, route }) => ({
+          headerShown: false,
+        })}
+      />
+      <Stack.Screen
+        name="ChatScreen"
+        component={ChatStackScreen}
+        options={({ navigation, route }) => ({
+          headerShown: true,
+          headerTitleAlign: "center",
+          headerTitleStyle: {
+            fontSize: 15
+          },
+          headerTitle: "Veganee",
+        })}
+      />
 
-        </Stack.Navigator>
-    )
+    </Stack.Navigator>
+  )
 }
 
 const CameraStackScreen = () => {
-    return(
-      <Stack.Navigator>
-        <CameraStack.Screen 
-          name = 'RectangleScanner'
-          options={({navigation, route}) => ({
-            headerShown: true,
-            headerTransparent: true,
-            headerTitle: '',
-          })}
-          component = {RectangleScannerScreen} />
-      </Stack.Navigator>
-    )
-  }
+  return (
+    <Stack.Navigator>
+      <CameraStack.Screen
+        name='RectangleScanner'
+        options={({ navigation, route }) => ({
+          headerShown: true,
+          headerTransparent: true,
+          headerTitle: '',
+        })}
+        component={RectangleScannerScreen} />
+    </Stack.Navigator>
+  )
+}
 
-  const ChatStackScreen = () => {
-    return(
-      <Stack.Navigator>
-        <ChatStack.Screen 
-          name = "Chat" 
-          component={Chat}
-          options = {(navigation, route)=> ({
-            headerShown: false,
-            headerTitle: '',
-            tabBarStyle: {display: 'none'},
-          })}
-           />
-      </Stack.Navigator>
-    );
-  };
+const ChatStackScreen = () => {
+  return (
+    <Stack.Navigator>
+      <ChatStack.Screen
+        name="Chat"
+        component={Chat}
+        options={(navigation, route) => ({
+          headerShown: false,
+          headerTitle: '',
+          tabBarStyle: { display: 'none' },
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default RootStack;
